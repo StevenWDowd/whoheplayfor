f6a4635e0dfc037bb478e560fc82f4ba2d7c5f25
Steven Dowd <swdowd@gmail.com>
2024-05-08 22:04:01 -0400

    initial commit

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+#add api variables here
+
+#Routes#
+
+#GETs
+
+#landing page/begin game
+
+#player data page(name, picture, year)
+
+#results page (correct or incorrect)
+
+#POSTs
+
+#send answer
