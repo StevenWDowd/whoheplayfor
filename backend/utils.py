d9719a879d26de4a28ba8e2208693284ca04568d
Steven Dowd <swdowd@gmail.com>
2024-05-14 01:15:38 -0400

    Added function to get data for a season of a player's career

--- backend/utils.py
+++ backend/utils.py
@@ -2,8 +2,11 @@ from nba_api.stats.static import players
 from nba_api.stats.static import teams
 from nba_api.stats.endpoints import playercareerstats
 import pydash
+import pandas
 
 all_players = players.get_players()
+#all_teams = teams.get_teams()
+headshot_url_stub = 'https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/'
 
 def get_random_player():
     """Retrieves a random player from NBA history.
@@ -17,5 +20,62 @@ def get_random_player():
     target_player = pydash.sample(all_players)
     return target_player
 
-def get_player_seasons(id):
-    """"""
+def get_player_season(id):
+    """Takes a player ID and returns a dict representing a season from that
+    player's career, like so:
+
+
+    'PLAYER_ID': [201167],
+    'SEASON_ID': ['2015-16'],
+    'LEAGUE_ID': ['00'],
+    'TEAM_ID': [1610612752],
+    'TEAM_ABBREVIATION': ['NYK'],
+    'PLAYER_AGE': [30.0],
+    'GP': [71],
+    'GS': [57],
+    'MIN': [2371.0],
+    'FGM': [354],
+    'FGA': [799],
+    'FG_PCT': [0.443],
+    'FG3M': [91],
+    'FG3A': [238],
+    'FG3_PCT': [0.382],
+    'FTM': [110],
+    'FTA': [131],
+    'FT_PCT': [0.84],
+    'OREB': [23],
+    'DREB': [243],
+    'REB': [266],
+    'AST': [144],
+    'STL': [25],
+    'BLK': [10],
+    'TOV': [82],
+    'PF': [142],
+    'PTS': [909]}"""
+
+    career = playercareerstats.PlayerCareerStats(player_id=id)
+
+    seasons_table = career.get_data_frames[0]
+
+    season = seasons_table.sample().to_dict('list')
+
+    return season
+
+#def get_team(id):
+    """Takes a team ID and returns a team dict like so:
+    {'id': 1610612752,
+    'full_name': 'New York Knicks',
+    'abbreviation': 'NYK',
+    'nickname': 'Knicks',
+    'city': 'New York',
+    'state': 'New York',
+    'year_founded': 1946}"""
+
+def generate_question_data(player, season):
+
+
+
+
+
+
+
