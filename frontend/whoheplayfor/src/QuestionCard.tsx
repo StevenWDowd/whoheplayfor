5696015a48a98781a138c9bb3b0400f453fd4e84
Steven Dowd <swdowd@gmail.com>
2024-05-20 17:00:56 -0400

    minor changes

--- frontend/whoheplayfor/src/QuestionCard.tsx
+++ frontend/whoheplayfor/src/QuestionCard.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { PlayerInterface } from "./utils";
+import TeamButton from "./TeamButton";
+import TeamChoiceBar from "./TeamChoiceBar";
 
 function QuestionCard(player: PlayerInterface){
 
--- frontend/whoheplayfor/src/TeamChoiceBar.tsx
+++ frontend/whoheplayfor/src/TeamChoiceBar.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
+import TeamButton from "./TeamButton";
 
 function TeamChoiceBar(){
     const [formData, setFormData] = useState(null);
+
+    return (
+        <div></div>
+    )
 
 }
 
